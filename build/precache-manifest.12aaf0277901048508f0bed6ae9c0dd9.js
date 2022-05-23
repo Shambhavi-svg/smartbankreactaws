bdf45646e0f05e8461e3c905edfb2ab797e3e797
Shambhavi-svg <shambhavi175@gmail.com>
2022-05-23 16:40:13 +0530

    First Commit

---
+++ build/precache-manifest.12aaf0277901048508f0bed6ae9c0dd9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "514925eeb92bba63c526",
+    "url": "/static/css/main.36a5fea9.chunk.css"
+  },
+  {
+    "revision": "514925eeb92bba63c526",
+    "url": "/static/js/main.514925ee.chunk.js"
+  },
+  {
+    "revision": "3a03db0996de3a2ffc3d",
+    "url": "/static/css/1.caf6dc0b.chunk.css"
+  },
+  {
+    "revision": "3a03db0996de3a2ffc3d",
+    "url": "/static/js/1.3a03db09.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "daf79ecb2617e825a7bc8969be9e33b4",
+    "url": "/static/media/starbucks.daf79ecb.png"
+  },
+  {
+    "revision": "6995991dfa1cd6e905e9f84a95d37998",
+    "url": "/static/media/titan.6995991d.jpg"
+  },
+  {
+    "revision": "7f7e1f452d3152cab65574e192c65288",
+    "url": "/static/media/bookmyshow.7f7e1f45.png"
+  },
+  {
+    "revision": "6880c51b4ec73df257685e1ab6eb932f",
+    "url": "/static/media/flipkart.6880c51b.jpg"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/static/media/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "f0dac4726b93dc1a4a66ed0bb3e5320f",
+    "url": "/index.html"
+  }
+];
